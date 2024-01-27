63583163865f6db3b8ca9206935a040bcb9ccccb
Bauyrzhan <b.balgaziev03@gmail.com>
2024-01-27 14:03:39 +0600

    feat: init tortoise and aerich

--- code/app.py
+++ code/app.py
@@ -1,11 +1,28 @@
 import uvicorn
+from contextlib import asynccontextmanager
+
+from tortoise import Tortoise
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from code.config import settings
+from code.config import settings, TORTOISE_CONFIG
 from code.handlers import health
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        await Tortoise.init(config=TORTOISE_CONFIG)
+        yield
+    finally:
+        await Tortoise.close_connections()
+
+
+app = FastAPI(
+    title='Halyq Life Funnel API',
+    lifespan=lifespan,
+    openapi_url='/openapi.json/'
+)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
--- code/config.py
+++ code/config.py
@@ -1,8 +1,19 @@
-from pydantic import BaseSettings
-
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     debug: bool = True
+    database_url: PostgresDsn
 
 
 settings = Settings()
+
+TORTOISE_CONFIG = {
+    'connections': {'default': str(settings.database_url)},
+    'apps': {
+        'models': {
+            'models': ['aerich.models', 'code.models'],
+            'default_connection': 'default',
+        },
+    },
+}
