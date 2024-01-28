d8f735721098d460bb399aa7f28b5439e00117f3
Bauyrzhan <b.balgaziev03@gmail.com>
2024-01-28 09:33:28 +0600

    feat: favicon and logo added

--- code/config.py
+++ code/config.py
@@ -6,18 +6,25 @@ class Settings(BaseSettings):
     debug: bool = True
     database_url: PostgresDsn
     redis_url: str
-    admin_logo_url: str = "https://preview.tabler.io/static/logo-white.svg"
-    login_logo_url: str = "https://preview.tabler.io/static/logo.svg"
+    admin_logo_url: str = (
+        "https://www.halyklife.kz/themes/halyk/assets/images/svg/logo.svg"
+    )
+    login_logo_url: str = (
+        "https://www.halyklife.kz/themes/halyk/assets/images/svg/logo.svg"
+    )
+    favicon_url: str = (
+        "https://www.halyklife.kz/themes/halyk/assets/favicon/favicon-32x32.png"
+    )
 
 
 settings = Settings()
 
 TORTOISE_CONFIG = {
-    'connections': {'default': str(settings.database_url)},
-    'apps': {
-        'models': {
-            'models': ['aerich.models', 'code.models'],
-            'default_connection': 'default',
+    "connections": {"default": str(settings.database_url)},
+    "apps": {
+        "models": {
+            "models": ["aerich.models", "code.models"],
+            "default_connection": "default",
         },
     },
 }
